6c07929b0d750676bbdffa446c1fe17f6376db15
Ricardo Contreras <99054420+racontrz@users.noreply.github.com>
2023-09-04 14:02:36 +0000

    se genera componente formularios

---
+++ src/component/views/otros/Formularios.jsx
@@ -0,0 +1,41 @@
+import { Container } from "react-bootstrap";
+import Accordion from "react-bootstrap/Accordion";
+
+function Formularios() {
+  return (
+    <div className="container mt-3">
+      <Accordion defaultActiveKey="1" className="mb-5">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Formularios</Accordion.Header>
+          <Accordion.Body>
+            <div>
+              <p>
+                <a
+                  href="https://drive.google.com/file/d/1Wl5a-AcTLzioQ1rQlLZ74gcwbauw2R6o/view?usp=drive_link"
+                  target="_blank"
+                >
+                  Renuncia Móvil
+                </a>
+              </p>
+            </div>
+            <div>
+              <p>
+                <a
+                  href="https://docs.google.com/document/d/10gbMsZX-8ABFWcDV6nPk8ka166hDB-0H/edit?usp=drive_link&ouid=108019970938918647926&rtpof=true&sd=true"
+                  target="_blank"
+                >
+                  Formulario de Reclamos
+                </a>
+              </p>
+            </div>
+            <div className="me-4 mt-3 text-end fs-6">
+              {/* <p>Informacion al 01/07/2023</p> */}
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+}
+
+export default Formularios;
