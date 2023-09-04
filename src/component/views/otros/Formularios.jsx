ca12f01843165a458b559aedbf0ae709b50b41ae
Ricardo Contreras <99054420+racontrz@users.noreply.github.com>
2023-09-04 14:17:33 +0000

    se agregan botones

--- src/component/views/otros/Formularios.jsx
+++ src/component/views/otros/Formularios.jsx
@@ -14,7 +14,8 @@ function Formularios() {
                   href="https://drive.google.com/file/d/1Wl5a-AcTLzioQ1rQlLZ74gcwbauw2R6o/view?usp=drive_link"
                   target="_blank"
                 >
-                  Renuncia Móvil
+                  <button type="button" class="btn btn-outline-primary">Renuncia Móvil</button>
+
                 </a>
               </p>
             </div>
@@ -24,7 +25,8 @@ function Formularios() {
                   href="https://docs.google.com/document/d/10gbMsZX-8ABFWcDV6nPk8ka166hDB-0H/edit?usp=drive_link&ouid=108019970938918647926&rtpof=true&sd=true"
                   target="_blank"
                 >
-                  Formulario de Reclamos
+                  <button type="button" class="btn btn-outline-primary">Formulario de Reclamos
+</button>
                 </a>
               </p>
             </div>
