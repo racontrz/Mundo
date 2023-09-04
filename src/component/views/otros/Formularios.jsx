cb56afad8de03814627e9e569229879146ea746e
Ricardo Contreras <99054420+racontrz@users.noreply.github.com>
2023-09-04 14:24:59 +0000

    se agrega flex a los botones

--- src/component/views/otros/Formularios.jsx
+++ src/component/views/otros/Formularios.jsx
@@ -8,14 +8,16 @@ function Formularios() {
         <Accordion.Item eventKey="0">
           <Accordion.Header>Formularios</Accordion.Header>
           <Accordion.Body>
+            <div className="dd-flex flex-wrap">
             <div>
               <p>
                 <a
                   href="https://drive.google.com/file/d/1Wl5a-AcTLzioQ1rQlLZ74gcwbauw2R6o/view?usp=drive_link"
                   target="_blank"
                 >
-                  <button type="button" class="btn btn-outline-primary">Renuncia Móvil</button>
-
+                  <button type="button" class="btn btn-outline-primary">
+                    Renuncia Móvil
+                  </button>
                 </a>
               </p>
             </div>
@@ -25,10 +27,12 @@ function Formularios() {
                   href="https://docs.google.com/document/d/10gbMsZX-8ABFWcDV6nPk8ka166hDB-0H/edit?usp=drive_link&ouid=108019970938918647926&rtpof=true&sd=true"
                   target="_blank"
                 >
-                  <button type="button" class="btn btn-outline-primary">Formulario de Reclamos
-</button>
+                  <button type="button" class="btn btn-outline-primary">
+                    Formulario de Reclamos
+                  </button>
                 </a>
               </p>
+            </div>
             </div>
             <div className="me-4 mt-3 text-end fs-6">
               {/* <p>Informacion al 01/07/2023</p> */}
